1889c72fcf378cd3610daa2e565c6122e846beb0
Ebad Kamil <ebad.kamil@nexigroup.com>
2023-12-30 16:19:19 +0100

    Add minimal Connector class

--- src/connectors/connection.py
+++ src/connectors/connection.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 from sqlmodel import Session
 
 from src.connectors.engine_factory import EngineFactory
+from src.connectors.constants import DbTypes
 
 
 class Connector:
-    def __init__(self, db_type: ) -> None:
-        pass
+    def __init__(self, db_type: DbTypes, authentication: Path) -> None:
+        self._engine = EngineFactory.from_database_type(db_type)(authentication).get_engine()
