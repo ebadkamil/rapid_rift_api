c59d8466ff41cc39dbe69f98779e90962e98c6fa
Ebad Kamil <ebad.kamil@nexigroup.com>
2023-12-31 15:49:29 +0100

    Unittest: Add test to cover UNKNOWN dbtype

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -1,6 +1,16 @@
+import pytest
+
+from src.connectors.connection import Connector
+from src.connectors.constants import DbTypes
+
+
+
 def test_connector_returns_correct_engine_for_db_type():
     assert True
 
 
 def test_connector_returns_not_implemented_error_for_unknown_db_type():
-    assert False
+    with pytest.raises(NotImplementedError) as exc_info:
+        Connector(DbTypes.UNKNOWN, "/PATH/TO/AUTHENTICATION")
+
+    assert str(exc_info.value) == "Engine type DbTypes.UNKNOWN not implemented"
