86c117c3fb4114f7208a2ca9f75a9336f892563f
Ebad Kamil <ebad.kamil@nexigroup.com>
2024-01-01 01:40:26 +0100

    Unittest: Add temporary authentication file fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,23 @@
+import tempfile
+from pathlib import Path
+
 import pytest
+import yaml
 
 
-@pytest.fixture(scope="function")
-def test_dataframe():
-    data = [
-        ("a", "aa", 1.0, 2.0, 3.0, 4.0),
-        ("b", "bb", -1.0, -2.0, -3.0, -4.0),
-        ("c", "cc", 5.0, 6.0, 7.0, 8.0),
-    ] * 10
+@pytest.fixture
+def temp_authentication_file():
+    temp_file = tempfile.NamedTemporaryFile(mode="w", delete=False)
+    auth_data = {
+        "user": "test_user",
+        "password": "test_pass",
+        "host": "test_host",
+        "port": "test_port",
+        "service": "test_service",
+        "encoding": "test_encoding",
+    }
+    yaml.dump(auth_data, temp_file)
+    temp_file.close()
 
-    return data
+    yield Path(temp_file.name)
+    Path(temp_file.name).unlink()
