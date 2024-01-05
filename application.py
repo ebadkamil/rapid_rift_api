570941a2f2c7da6e66ef95d6a9a9c308510dec85
Ebad Kamil <ebad.kamil@nexigroup.com>
2024-01-05 23:55:51 +0100

    Use DBtypes in typer command

--- application.py
+++ application.py
@@ -1,16 +1,22 @@
 from pathlib import Path
+
 import typer
 
 from src.connectors.connection import Connector
 from src.connectors.constants import DbTypes
 
-
 app = typer.Typer()
 
 
 @app.callback()
-def main(ctx: typer.Context, db_type: str, authentication: Path = typer.Argument(..., exists=True, file_okay=True, readable=True)):
-    ctx.obj = Connector(DbTypes[db_type.upper()], authentication)
+def set_context_object(
+    ctx: typer.Context,
+    db_type: DbTypes,
+    authentication: Path = typer.Argument(
+        ..., exists=True, file_okay=True, readable=True
+    ),
+):
+    ctx.obj = Connector(db_type, authentication)
 
 
 @app.command()
