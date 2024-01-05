803eec72181b0bfa3122666f6683bf1bd2835972
Ebad Kamil <ebad.kamil@nexigroup.com>
2024-01-05 23:39:55 +0100

    Use typer context in cli

--- application.py
+++ application.py
@@ -8,10 +8,14 @@ from src.connectors.constants import DbTypes
 app = typer.Typer()
 
 
+@app.callback()
+def main(ctx: typer.Context, db_type: str, authentication: Path = typer.Argument(..., exists=True, file_okay=True, readable=True)):
+    ctx.obj = Connector(DbTypes[db_type.upper()], authentication)
+
+
 @app.command()
-def get_table_names(db_type: str, authentication: Path = typer.Argument(..., exists=True, file_okay=True, readable=True)):
-    conn = Connector(DbTypes.ORACLE, authentication)
-    print(conn.get_table_names())
+def get_table_names(ctx: typer.Context):
+    print(ctx.obj.get_table_names())
 
 
 if __name__ == "__main__":
