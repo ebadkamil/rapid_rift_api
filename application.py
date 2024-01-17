7ec11cc29a7fcea0e0029782755992b34956d578
Ebad Kamil <ebad.kamil@nexigroup.com>
2024-01-17 22:53:11 +0100

    Add minimal rest api

--- application.py
+++ application.py
@@ -33,5 +33,11 @@ def create_tables_from_data_models(ctx: typer.Context):
     SQLModel.metadata.create_all(ctx.obj.engine)
 
 
+@app.command()
+def start_server(host: str = typer.Option(default= "no2-pphdpgate-6", help="hostname"), port: int = typer.Option(default=8000, help="port number")):
+    import uvicorn
+    uvicorn.run("src.api.server_application:server", host=host, port=port, reload=True)
+
+
 if __name__ == "__main__":
     app()
