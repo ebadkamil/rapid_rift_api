929b50ea51800bbb64862d20938270ed9d45b025
Ebad Kamil <ebad.kamil@nexigroup.com>
2024-01-27 16:08:23 +0100

    Add dependency injection session and inspector

--- src/app/api/deps.py
+++ src/app/api/deps.py
@@ -3,10 +3,20 @@ from typing_extensions import Annotated
 
 from src.app.core.config import settings
 from src.app.db.connection import Connector
+from sqlmodel import Session, inspect
+
 
 connector = Connector(settings.DBTYPE)
 
 
 def get_session():
-    with connector.session as session:
+    with Session(connector.engine) as session:
         yield session
+
+
+def get_inspector():
+    yield connector.inspector
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+InsepectDep = Annotated[inspect, Depends(get_inspector)]
