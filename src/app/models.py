50bcddabe8093e2e7cb39592d00d796487de5003
Ebad Kamil <ebad.kamil@nexigroup.com>
2024-01-29 18:03:34 +0100

    Add user data model

--- src/app/models.py
+++ src/app/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from enum import Enum
-from typing import Optional
+from typing import Optional, Union
 
 from sqlmodel import Column, Enum, Field, SQLModel
 
@@ -19,7 +19,6 @@ class Product_Type(SQLModel, table=True):
 class Production_Line(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     line_number: int
-    status: Status
     last_update_timestamp: datetime
 
 
@@ -33,3 +32,15 @@ class Mapping(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     line_id: Optional[int] = Field(default=None, foreign_key="production_line.id")
     order_id: Optional[int] = Field(default=None, foreign_key="order.id")
+
+
+class User(SQLModel):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+class UserInDb(User):
+    password: str
