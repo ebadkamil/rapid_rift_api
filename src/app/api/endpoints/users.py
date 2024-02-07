21590e71064b7604c486fc0b39de1ff6a71fac5f
Ebad Kamil <ebad.kamil@nexigroup.com>
2024-02-07 12:46:29 +0100

    Add password hashing utils in crud

--- src/app/api/endpoints/users.py
+++ src/app/api/endpoints/users.py
@@ -2,19 +2,13 @@ from fastapi import APIRouter, HTTPException
 
 from src.app.api.deps import InsepectDep, SecurityDep, SessionDep
 from src.app.models import UserCreate
-
+from src.app.crud.users import create_user
 
 router = APIRouter()
 
 
 @router.post("/open")
 def create_open_user(user: UserCreate, session: SessionDep):
-    user = crud.get_user_by_email(session=session, email=user_in.email)
-    if user:
-        raise HTTPException(
-            status_code=400,
-            detail="The user with this username already exists in the system",
-        )
-    user_create = UserCreate.from_orm(user_in)
-    user = crud.create_user(session=session, user_create=user_create)
+    user_create = UserCreate.from_orm(user)
+    user = create_user(session=session, user_create=user_create)
     return user
