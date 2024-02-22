ff196d374ce7c13c6ab0a1489473bd422d4dc4e9
Ebad Kamil <ebad.kamil@nexigroup.com>
2024-02-22 15:40:52 +0100

    Add jwt token create utility function

---
+++ src/app/utils/__init__.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta, timezone
+from typing import Annotated, Union
+
+from jose import JWTError, jwt
+
+from src.app.core.config import settings
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    to_encode = {"exp": expire, "sub": str(data)}
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+    print("Encoded JWT token: ", encoded_jwt)
+    return encoded_jwt
